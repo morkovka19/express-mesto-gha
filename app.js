8fbdd8c9fb3fd7c7208bc20c23843a25127ac6a5
Polina Shapovalova <shapovalova.p.d@gmail.com>
2023-09-24 17:05:30 +0300

    fix bugs

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post('/signin', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2),
-    avatar: Joi.string().url(),
+    avatar: Joi.string(),
     password: Joi.string().required().min(8),
     email: Joi.string().required().email(),
   }),
